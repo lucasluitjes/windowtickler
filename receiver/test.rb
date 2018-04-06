24f6ffb915d8e982026c717231725f3e928c3c5e
Lucas Luitjes <lucas@luitjes.it>
2018-04-06 18:16:17 +0200

    bug

--- receiver/test.rb
+++ receiver/test.rb
@@ -6,7 +6,7 @@ RATIO_X, RATIO_Y = 1000.0/RES_X, 1000.0/RES_Y
 server = WEBrick::HTTPServer.new :Port => 8000
 
 server.mount_proc '/' do |req, res|
-  x, y = req.query[x].to_i, req.query[y].to_i
+  x, y = req.query["x"].to_i, req.query["y"].to_i
   x, y = (x * RATIO_X).to_i, (y * RATIO_Y).to_i
   `xdotool mousemove #{x} #{y}`
   res.body = 'ok'
