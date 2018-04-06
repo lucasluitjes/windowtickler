aa2ae80e6b82299b0fbf291315723265fc031a0e
Lucas Luitjes <lucas@luitjes.it>
2018-04-06 18:12:42 +0200

    fix ratio

--- receiver/test.rb
+++ receiver/test.rb
@@ -1,9 +1,14 @@
 require 'webrick'
 
+RES_X, RES_Y = *`xdpyinfo`.scan(/(\d+)x(\d+) pixels/).flatten.map(&:to_i)
+RATIO_X, RATIO_Y = 1000.0/RES_X, 1000.0/RES_Y
+
 server = WEBrick::HTTPServer.new :Port => 8000
 
 server.mount_proc '/' do |req, res|
-  `xdotool mousemove #{req.query['x']} #{req.query['y']}`
+  x, y = req.query[x].to_i, req.query[y].to_i
+  x, y = (x * RATIO_X).to_i, (y * RATIO_Y).to_i
+  `xdotool mousemove #{x} #{y}`
   res.body = 'ok'
 end
 
--- sender/test.rb
+++ sender/test.rb
@@ -3,8 +3,12 @@ require 'net/http'
 
 BASE = ARGV[0]
 
+RES_X, RES_Y = *`xdpyinfo`.scan(/(\d+)x(\d+) pixels/).flatten.map(&:to_i)
+RATIO_X, RATIO_Y = RES_X/1000.0, RES_Y/1000.0
+
 def send_location location
-  x, y = *location.scan(/x:(\d+) y:(\d+)/).flatten
+  x, y = *location.scan(/x:(\d+) y:(\d+)/).flatten.map(&:to_i)
+  x, y = (x * RATIO_X).to_i, (y * RATIO_Y).to_i
 
   Net::HTTP.get(URI("#{BASE}/mouse?x=#{x}&y=#{y}"))
 end
